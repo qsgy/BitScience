f111808fb86faed22593c60dd30552d3dbbcc737
浩 欧阳 <961390320@qq.com>
2023-06-21 19:41:07 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BitScience
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
