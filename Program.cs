34826c58c086b5d9be920d0a2e2e07081b4050b5
浩 欧阳 <961390320@qq.com>
2023-06-25 19:06:57 +0800

    ToBinStr

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(((byte)(0xff&0xff)).ToBinStr());
+            Console.WriteLine(((byte)(0xff&0x00)).ToBinStr());
+            Console.WriteLine(((byte)(0xff&0x01)).ToBinStr());
+        }
+    }
+    public static class helper
+    {
+        public static string ToBinStr(this byte data)
+        {
+            return Convert.ToString(data,2).PadLeft(8,'0');
         }
     }
 }
